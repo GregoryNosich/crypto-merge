be6f824beccc25c9a89dd81a9e441f97ca1b2442
Gregory Nosich <gregorynosv@gmail.com>
2024-05-19 18:04:39 +0300

    Added restart button

--- Assets/Scripts/GameOverText.cs
+++ Assets/Scripts/GameOverText.cs
@@ -7,11 +7,9 @@ using TMPro;
 public class GameOverText : MonoBehaviour
 {
     public TextMeshProUGUI myText;
-    public TextMeshProUGUI restartText;
 
     void Update()
     {
         myText.enabled = CollisionController.gameOver;
-        restartText.enabled = CollisionController.gameOver;
     }
 }
---
+++ Assets/Scripts/RestartButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+        CollisionController.gameOver = false;
+        ScoreCounter.Score = 0;
+    }
+}
